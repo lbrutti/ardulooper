eefeb87cd218b50ad994c78706d2cff58b894703
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2023-06-21 16:16:50 +0200

    added random button

--- ArduLooper.ino
+++ ArduLooper.ino
@@ -4,15 +4,16 @@
   Controls a cheap Ammoon Loop Station (https://www.ammoon.com/p-i1995.html)
 
   Three latch buttons are used to create "shortcuts" to play/rec, stop and flush footswitch combinations.
-  A pair of wires must be soldered on the two contacts of the original footswitch and connected to the 
+  A pair of wires must be soldered on the two contacts of the original footswitch and connected to the
   collector and emitter of an NPN transistor.
-  
+
 
 */
 #define LOOPER   2
 #define FLUSH   14
 #define STOP  15
 #define TOGGLE  16
+#define RANDOMIZE  17
 
 // the setup function runs once when you press reset or power the board
 void setup() {
@@ -23,6 +24,7 @@ void setup() {
   pinMode(FLUSH, INPUT_PULLUP);
   pinMode(STOP, INPUT_PULLUP);
   pinMode(TOGGLE, INPUT_PULLUP);
+  pinMode(RANDOMIZE, INPUT_PULLUP);
 
   Serial.begin(9600);
 
@@ -30,26 +32,32 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-
-  if (digitalRead(FLUSH) == LOW) {
-    Serial.println("FLUSHING...");
-    clear();
-    Serial.println("FLUSHED!");
-
-  }
-
-  else if (digitalRead(STOP) == LOW) {
-    Serial.println("STOPPING...");
-    stop();
-    Serial.println("STOPPED!");
-
-  }
-  else if (digitalRead(TOGGLE) == LOW) {
-    Serial.println("TOGGLING...");
+  if (digitalRead(RANDOMIZE) == LOW) {
+    Serial.println("RANDOMIZE...");
     press(25);
-    delay(500);
-    Serial.println("TOGGLED!");
+    delay(random(2000));
+    Serial.println("RANDOMIZE!");
+  } else {
+    if (digitalRead(FLUSH) == LOW) {
+      Serial.println("FLUSHING...");
+      clear();
+      Serial.println("FLUSHED!");
+
+    }
+    else if (digitalRead(STOP) == LOW) {
+      Serial.println("STOPPING...");
+      stop();
+      Serial.println("STOPPED!");
+
+    }
+    else if (digitalRead(TOGGLE) == LOW) {
+      Serial.println("TOGGLING...");
+      press(25);
+      delay(500);
+      Serial.println("TOGGLED!");
+    }
   }
+
 }
 
 void press(int time) {
