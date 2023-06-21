2d5d1cbde5fb40dafe68e0617fb5054e22dad811
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2023-06-21 12:14:17 +0200

    added comments

--- ArduLooper.ino
+++ ArduLooper.ino
@@ -1,25 +1,13 @@
 /*
-  Blink
+  ArduLooper
 
-  Turns an LED on for one second, then off for one second, repeatedly.
+  Controls a cheap Ammoon Loop Station (https://www.ammoon.com/p-i1995.html)
 
-  Most Arduinos have an on-board LED you can control. On the UNO, MEGA and ZERO
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN is set to
-  the correct LED pin independent of which board is used.
-  If you want to know what pin the on-board LED is connected to on your Arduino
-  model, check the Technical Specs of your board at:
-  https://www.arduino.cc/en/Main/Products
+  Three latch buttons are used to create "shortcuts" to play/rec, stop and flush footswitch combinations.
+  A pair of wires must be soldered on the two contacts of the original footswitch and connected to the 
+  collector and emitter of an NPN transistor.
+  
 
-  modified 8 May 2014
-  by Scott Fitzgerald
-  modified 2 Sep 2016
-  by Arturo Guadalupi
-  modified 8 Sep 2016
-  by Colby Newman
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/Blink
 */
 #define LOOPER   2
 #define FLUSH   14
